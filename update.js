b3a7612dea4801ab9f1506e882ce521c6a459b7b
transiscodev <sirgjw@gmail.com>
2019-09-18 18:36:11 -0700

    keep refreshing

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var thehtml = `<div data-role="collapsible" data-collapsed-icon="plus" data-expanded-icon="check" data-collapsed="false">
 <h3>Recent Updates (Online)</h3>
-   <p>You are connected to the internet. INSTANT UPDATES? The most recent updates will appear here. No updates at this time.</p></div>`;
+   <p>You are connected to the internet. Keep refreshing! The most recent updates will appear here. No updates at this time.</p></div>`;
 
 document.getElementById("thesub").innerHTML = thehtml;
