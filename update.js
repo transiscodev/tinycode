06b037925ece06a6e19eb3ee66893bd2c9efc5b9
transiscodev <sirgjw@gmail.com>
2019-09-20 10:21:35 -0700

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,5 @@
-var thehtml = `<div data-role="collapsible" data-collapsed-icon="plus" data-expanded-icon="check" data-collapsed="false">
-<h3>Recent Updates (Online)</h3>
-   <p>No updates at this time.</p></div>`;
+var thehtml = `<h3>Recent Updates (Online)</h3>
+   <p>No updates at this time.</p>`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
