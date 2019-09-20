938e185d5240bfdae5da6eea15baa91e1297fbfa
transiscodev <sirgjw@gmail.com>
2019-09-19 21:41:50 -0700

    finalize

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var thehtml = `<div data-role="collapsible" data-collapsed-icon="plus" data-expanded-icon="check" data-collapsed="false">
 <h3>Recent Updates (Online)</h3>
-   <p>You are connected to the internet. If your internet connection is not working, switch your device to flight mode to use this app offline. The most recent updates will appear here. There are no updates at this time.</p></div>`;
+   <p>No updates at this time.</p></div>`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
