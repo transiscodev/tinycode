7bf5da3dbd136e4a85082d5331303ce0bb3539dd
transiscodev <sirgjw@gmail.com>
2019-11-08 21:22:55 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var thehtml = `<h3>Recent Updates (Online)</h3>
    <h4>URGENT UPDATE<BR>
-We are on a "C" schedule regardless of the fire incident.</h4>`;
+No school Monday.</h4>`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
