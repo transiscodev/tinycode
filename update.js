4b1b278f9e35a5ea8f415c71025c9bbefd2651f6
transiscodev <sirgjw@gmail.com>
2019-11-12 12:35:57 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,13 +1,13 @@
 var thehtml = `<h3>Recent Updates (Online)</h3>
    <h4>
 Special Schedule Week:<br>
-<li>
-<ul>Monday    NO SCHOOL</ul>
-<ul>Tuesday  'A' Schedule</ul>
-<ul>Wendsday 'B' Schedule</ul>
-<ul>Thursday 'C' Schedule with Bear Time</ul>
-<ul>Friday   'C' Schedule with make up Vet's Assembly</ul>
-</li>
+<ul>
+<li>Monday    NO SCHOOL</li>
+<li>Tuesday  'A' Schedule</li>
+<li>Wendsday 'B' Schedule</li>
+<li>Thursday 'C' Schedule with Bear Time</li>
+<li>Friday   'C' Schedule with make up Vet's Assembly</li>
+</ul>
 </h4>`;
 
 document.getElementById("thesub").innerHTML = thehtml;
