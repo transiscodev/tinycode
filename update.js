e398e4124be811e9bb050121023c00a2d9ab779a
transiscodev <sirgjw@gmail.com>
2019-11-15 09:01:56 -0800

    Update update.js

--- update.js
+++ update.js
@@ -6,10 +6,116 @@ Special Schedule Week:<br>
 <li>Tuesday  'A' Schedule</li>
 <li>Wednesday 'B' Schedule</li>
 <li>Thursday 'C' Schedule with Bear Time</li>
-<li>Friday   'OlyWay' Schedule with make up Vet's Assembly<br>
-Assembly immediately follows Period 2.<br>
-Break will occur after assembly.<br>
-Full Schedule will be posted shortly. </li>
+<li>Friday   'MODIFIED OlyWay' Schedule with make up Vet's Assembly<br>
+<h1>Modified OlyWay Scheule for Vet's Assembly</h1>
+<table data-role="table" id="my-table" data-mode="reflow">
+<tbody>
+<tr>
+<td rowspan="2">
+<h3>Class</h3>
+</td>
+</tr>
+<tr>
+<td>&nbsp;Time</td>
+</tr>
+<tr>
+<td>
+<p>Period 1</p>
+</td>
+<td>
+<p>7:45-8:45 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 2</p>
+</td>
+<td>
+<p>8:50-9:33 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Bear Time A</p>
+</td>
+<td>
+<p>9:43-10:08 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Bear Time B</p>
+</td>
+<td>
+<p>10:13-10:38 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Break</p>
+</td>
+<td>
+<p>9:33-9:43 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 3</p>
+</td>
+<td>
+<p>10:43-11:26 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Lunch 1</p>
+</td>
+<td>
+<p>11:28-11:58 a.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 4B</p>
+</td>
+<td>
+<p>12:03 p.m.-12:46 p.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 4A</p>
+</td>
+<td>
+<p>11:31 a.m.-12:14 p.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Lunch 2</p>
+</td>
+<td>
+<p>12:16-12:46 p.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 5</p>
+</td>
+<td>
+<p>12:51-1:34 p.m.</p>
+</td>
+</tr>
+<tr>
+<td>
+<p>Period 6</p>
+</td>
+<td>
+<p>1:39-2:22 p.m.</p>
+</td>
+</tr>
+</tbody>
+</table></li>
 </ul>
 <br> <I>No Bear Crew meeting on Thursday or Friday.</I>
 </h4>`;
