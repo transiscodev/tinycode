0f616abc9c2a872f2439a8b7cd73bbe56cbd4144
transiscodev <sirgjw@gmail.com>
2019-11-15 08:29:24 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,12 +1,12 @@
 var thehtml = `<h3>Recent Updates (Online)</h3>
-   <h4>
+   <h4>UPDATED<br>
 Special Schedule Week:<br>
 <ul>
 <li>Monday    NO SCHOOL</li>
 <li>Tuesday  'A' Schedule</li>
 <li>Wednesday 'B' Schedule</li>
 <li>Thursday 'C' Schedule with Bear Time</li>
-<li>Friday   'C' Schedule with make up Vet's Assembly</li>
+<li>Friday   'OlyWay' Schedule with make up Vet's Assembly</li>
 </ul>
 <br> <I>No Bear Crew meeting on Thursday or Friday.</I>
 </h4>`;
