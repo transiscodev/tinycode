824c8870ef9bd53e8a386b54b64957394c824deb
transiscodev <sirgjw@gmail.com>
2019-11-16 10:59:19 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,9 @@
-var thehtml = `<h3>Recent Updates (Online)</h3>
+var thehtml = `
+<link rel="stylesheet" href="https://bears23.bitbucket.io/jquery.mobile-1.2.1.min.css" />
+<link rel="stylesheet" href="https://bears23.bitbucket.io/jquery.mobile.structure-1.2.1.min.css" />
+<script src="https://bears23.bitbucket.io/jquery-1.8.3.min.js"></script>
+<script src="https://bears23.bitbucket.io/jquery.mobile-1.2.1.min.js"></script>
+<h3>Recent Updates (Online)</h3>
 Tonight is the final night of "TRAP", the Autumn play at OHS.<br>
 Come find out what the secret is at this final performance.<br>
 Tickets are on sale online until 7:30 PM. Doors open at 7 PM and the perfromace starts at 7:30 PM.<br>
