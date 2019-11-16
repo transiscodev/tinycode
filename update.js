8b2201b6199870a0987b5e18cd5500968ab57912
transiscodev <sirgjw@gmail.com>
2019-11-16 11:17:53 -0800

    Update update.js

--- update.js
+++ update.js
@@ -3,7 +3,54 @@ var thehtml = `
 Tonight is the final night of "TRAP", the Autumn play at OHS.<br>
 Come find out what the secret is at this final performance.<br>
 Tickets are on sale online until 7:30 PM. Doors open at 7 PM and the perfromace starts at 7:30 PM.<br>
-<a href="https://www.purplepass.com/#193941/Olympia_School_District_-_Theater_Productions-OHS_Theatre_Arts_Presents:_TRAP-Olympia_High_School-November-16-2019.html" data-theme="b" target="_blank" data-role="button">Buy or View Tickets</a>
+<a style="background-attachment: scroll;
+background-clip: border-box;
+background-color: rgb(57, 107, 158);
+background-image: linear-gradient(rgb(95, 156, 197), rgb(57, 107, 158));
+background-origin: padding-box;
+background-position: 0% 0%;
+background-position-x: 0%;
+background-position-y: 0%;
+background-repeat: repeat;
+background-size: auto;
+border-bottom-color: rgb(4, 64, 98);
+border-bottom-style: solid;
+border-bottom-width: 0px;
+border-image-outset: 0;
+border-image-repeat: stretch;
+border-image-slice: 100%;
+border-image-source: none;
+border-image-width: 1;
+border-left-color: rgb(4, 64, 98);
+border-left-style: solid;
+border-left-width: 1px;
+border-right-color: rgb(4, 64, 98);
+border-right-style: solid;
+border-right-width: 1px;
+border-top-color: rgb(4, 64, 98);
+border-top-style: solid;
+border-top-width: 1px;
+color: rgb(255, 255, 255);
+cursor: pointer;
+display: block;
+font-family: Helvetica, Arial, sans-serif;
+font-weight: 700;
+margin-bottom: 0px;
+margin-left: 0px;
+margin-right: 0px;
+margin-top: 0px;
+padding-bottom: 0px;
+padding-left: 0px;
+padding-right: 0px;
+padding-top: 0px;
+position: relative;
+text-align: center;
+text-decoration: none;
+text-decoration-color: rgb(255, 255, 255);
+text-decoration-line: none;
+text-decoration-style: solid;
+text-shadow: rgb(25, 75, 126) 0px 1px 1px;"
+href="https://www.purplepass.com/#193941/Olympia_School_District_-_Theater_Productions-OHS_Theatre_Arts_Presents:_TRAP-Olympia_High_School-November-16-2019.html" data-theme="b" target="_blank" data-role="button">Buy or View Tickets</a>
 `;
 
 document.getElementById("thesub").innerHTML = thehtml;
