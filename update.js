5bb7f6e965dd336e3d074aa9acf81d6f4d8bd531
transiscodev <sirgjw@gmail.com>
2019-11-16 13:24:54 -0800

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ var thehtml = `
 <h3>Recent Updates (Online)</h3>
 Tonight is the final night of "TRAP", the Autumn play at OHS. Come find out what the secret is at this final performance. Tickets are on sale online until 7:30 PM. Doors open at 7 PM and the perfromace starts at 7:30 PM.<br>
 <h2><a 
-href="https://www.purplepass.com/#193941/Olympia_School_District_-_Theater_Productions-OHS_Theatre_Arts_Presents:_TRAP-Olympia_High_School-November-16-2019.html" data-theme="b" data-role="button">Buy or View Tickets</a></h2>
+href="https://www.purplepass.com/#193941/Olympia_School_District_-_Theater_Productions-OHS_Theatre_Arts_Presents:_TRAP-Olympia_High_School-November-16-2019.html" rel="external" data-theme="b" data-role="button">Buy or View Tickets</a></h2>
 `;
 
 document.getElementById("thesub").innerHTML = thehtml;
