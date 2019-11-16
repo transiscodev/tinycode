3324964a0e25cb941c33a28eafc7b838f9695d9d
transiscodev <sirgjw@gmail.com>
2019-11-16 11:22:36 -0800

    Update update.js

--- update.js
+++ update.js
@@ -4,7 +4,7 @@ Tonight is the final night of "TRAP", the Autumn play at OHS.<br>
 Come find out what the secret is at this final performance.<br>
 Tickets are on sale online until 7:30 PM. Doors open at 7 PM and the perfromace starts at 7:30 PM.<br>
 <a style="
-height: 40x;
+height: 40px;
 background-attachment: scroll;
 background-clip: border-box;
 background-color: rgb(57, 107, 158);
