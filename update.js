d32d7b4c833d5d4a5a1b94dee30ab619b86cff34
transiscodev <sirgjw@gmail.com>
2019-11-22 08:35:22 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-<br>Regular schedule all week.`;
+Come shop the Bearzaar on Saturday!`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
