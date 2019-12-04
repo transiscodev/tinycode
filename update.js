be6e51f83b670289de16a1ba10a206baa1b4d57f
transiscodev <sirgjw@gmail.com>
2019-12-04 09:58:58 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,8 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
 Regular Schedule this week.
-<!-- Bidvertiser2015572 -->`;
+<!-- Bidvertiser2015572 -->
+<!-- AD VERIFICATION: This page is a backend for https://bears23.bitbucket.io I own both websites and can place verification code in the bitbucket site but I have to put the ads here in this page so it overrides the cacheing. -->`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
