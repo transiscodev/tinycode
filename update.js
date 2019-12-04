611a65dfcdc10b480719925211daa828970bf252
transiscodev <sirgjw@gmail.com>
2019-12-04 09:50:33 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,7 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-Regular Schedule this week.`;
+Regular Schedule this week.
+<!-- Bidvertiser2015570 -->`;
 
 document.getElementById("thesub").innerHTML = thehtml;
  
