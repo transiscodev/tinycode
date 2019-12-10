13f1973e69ef0244946556949fe9c89c3477d3ef
transiscodev <sirgjw@gmail.com>
2019-12-10 07:51:43 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-Regular Schedule this week.
+Regular Schedule this week. Video announcements temporarily delayed, today.
 <!-- Bidvertiser2015572 -->
 <!-- AD VERIFICATION: This page is a backend for https://bears23.bitbucket.io I own both websites and can place verification code in the bitbucket site but I have to put the ads here in this page so it overrides the cacheing. -->`;
 
