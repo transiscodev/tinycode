e59c9330d9422afd651b8fd4604a2b65a0dc617e
transiscodev <sirgjw@gmail.com>
2019-12-20 07:57:36 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-Regular Week`
+DUE TO EXTREME DELAYS, ALL STUDENTS ARE EXCUSED UNTIL 8:00 AM`
 document.getElementById("thesub").innerHTML = thehtml;
