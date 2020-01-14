0f08c6fabfb2ad667c67603b6282ab17146eae3c
transiscodev <sirgjw@gmail.com>
2020-01-14 10:03:45 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-<br> <img src="44D48707-A9ED-4315-BC76-2984717E219A.jpeg"><p>Schools and buses two hours late today, Tuesday, January 14. AM buses on snow routes. No out-of-district transportation. No AM preschool. No zero hour classes. No before-school activities. No Avanti HS program (periods 1-3). Breakfast and lunch will be served throughout the district. `
+<br> <img src="/44D48707-A9ED-4315-BC76-2984717E219A.jpeg"><p>Schools and buses two hours late today, Tuesday, January 14. AM buses on snow routes. No out-of-district transportation. No AM preschool. No zero hour classes. No before-school activities. No Avanti HS program (periods 1-3). Breakfast and lunch will be served throughout the district. `
 document.getElementById("thesub").innerHTML = thehtml;
