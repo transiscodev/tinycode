c02629336825efb132f34ce68537861d42a6d95b
transiscodev <sirgjw@gmail.com>
2020-01-14 09:45:00 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-OlyWay Schedule Today<br>Senior Meeting Bear Time B`
+<br> NO FORMAL SCHEDULE AT THIS TIME <p>Schools and buses two hours late today, Tuesday, January 14. AM buses on snow routes. No out-of-district transportation. No AM preschool. No zero hour classes. No before-school activities. No Avanti HS program (periods 1-3). Breakfast and lunch will be served throughout the district. `
 document.getElementById("thesub").innerHTML = thehtml;
