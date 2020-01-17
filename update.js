c310bfe1320bb5771bceab625c9f2a3ee6dcc5a0
transiscodev <sirgjw@gmail.com>
2020-01-17 09:41:14 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,152 +1,96 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
 <br> Due to icy roads, schools and buses are two hours late today, Friday, January 17. No out-of-district transportation. No zero hour classes. No before-school activities. No AM preschool. No Avanti HS AM tutoring or academic services. Buses will pick up students this morning on regular routes except for Routes 64 (Summit Lake) and Routes 50 (Cedar Flats), which are on snow routes. Breakfast and lunch will be served throughout the district. <br><h2><span style="font-size:24px">ORIGINAL SCHEDULE (NOT ACCOUNTING FOR LATE START)</span></h2>
-<h3><span style="font-size:16px">Friday, January 17, 2020<br>
-</span><span style="font-size:13px">
-DOUBLE ASSEMBLY SCHEDULE </span><br>
-<span style="font-size:13px">
-PERFORMING ARTS CENTER</span></h3>
-<table style="width:301px;height:170px" align="left" class=""
-cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-family:Montserrat;font-size:13px">Period 1</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;7:45 - 8:35<span
-class="Apple-tab-span" style="font-family:Arial"> </span>(50
-min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Assembly A<br>
-Period 2B</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px"><span style="font-size:13px"><span
-style="font-size:13px">&nbsp;8:40 &ndash; 9:30 (50 min.)</span><br>
-</span>&nbsp;9:35 - 10:25 (50 min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Period 2 A<br>
-Assembly B<br>
-</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px"><span style="font-size:13px"><span
-style="font-size:13px">&nbsp;8:40 - 9:30. (50 min.)</span><br>
-</span>&nbsp;9:35 &ndash; 10:25.(50 min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Break</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;10:25 &ndash; 10:35<span
-class="Apple-tab-span" style="font-family:Arial"> </span>(10
-min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Period 3</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;10:35 &ndash; 11:20<span
-class="Apple-tab-span" style="font-family:Arial"> </span>(45
-min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Lunch A<br>
-Period 4A</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;11:25 - 11:57 (32 min.)<br>
-&nbsp;11:57 - 12:42. (45 min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Period 4B<br>
-Lunch B<br>
-</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px"><span style="font-size:13px"><span
-style="font-size:13px">&nbsp;11:25 &ndash; 12:10<span
-class="Apple-tab-span"> </span>(45 min.)</span><br>
-</span>&nbsp;12:10 - 12:42<span class="Apple-tab-span"> </span>(32
-min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Period 5</span></p>
-</td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;12:47 - 1:32 (45 min.)</span></p>
-</td>
-</tr>
-<tr>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">Period 6</span></p> </td>
-<td class=""
-style="text-align:left;vertical-align:middle;white-space:nowrap;border:1px
-solid #a5a5a5">
-<p><span style="font-size:13px">&nbsp;1:37 - 2:22&nbsp;<span
-class="Apple-tab-span" style="font-family:Arial"> </span>(45
-min.)</span></p>
-</td>
-</tr>
-</tbody>
+<h2><span style="font-size: 24px;">MLK Assembly --2 hour Late Start (ice)</span></h2>
+<h3><span style="font-size: 16px;">Friday, January 17, 2020</span></h3>
+<table style="width: 301px; height: 170px;" align="left" class="" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-family: Montserrat; font-size: 13px;">Period 1</span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;9:45 - 10:16<span class="Apple-tab-span" style="font-family: Arial;">	</span>(31 min.)</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">Assembly<br>
+            </span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;"><span style="font-size: 13px;">&nbsp;10:21 &ndash; 11:11 (50 min.)</span><br>
+            </span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">Period 2<br>
+            </span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;"><span style="font-size: 13px;">&nbsp;11:16 - 11:42. (26 min.)</span><br>
+            </span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">Lunch 1</span></p>
+            <p><span style="font-size: 13px;">Period 3B</span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;11:42 &ndash; 12:17</span></p>
+            <p><span style="font-size: small; letter-spacing: normal; white-space: nowrap;">&nbsp;12:17 &ndash; 12:50 (33 min.)</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">Period 3A</span></p>
+            <p><span style="font-size: 13px;">Lunch 2</span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;11:47 &ndash; 12:17<span class="Apple-tab-span" style="font-family: Arial;">&nbsp;</span>(30 min.)</span></p>
+            <p><span style="font-size: small; letter-spacing: normal; white-space: nowrap;">&nbsp;12:17 &ndash; 12:50</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">Period 4</span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;12:55 - 1:21 (26 min.)<br>
+            </span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;"><span style="font-size: small; letter-spacing: normal; white-space: nowrap;">Period 5</span><br>
+            </span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: small; letter-spacing: normal; white-space: nowrap;">&nbsp;1:26 - 1:52 (26 min.)</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: small; letter-spacing: normal; white-space: nowrap;">Period 6</span></p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;1:57 - 2:22 (25 min.)</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p>&nbsp;</p>
+            </td>
+            <td class="" style="text-align: left; vertical-align: middle; white-space: nowrap; border: 1px solid rgb(165, 165, 165);">
+            <p><span style="font-size: 13px;">&nbsp;</span></p>
+            </td>
+        </tr>
+    </tbody>
 </table>
-<p>All Students <strong>report to class at the beginning of the
-period</strong> and leave belongings in locked classroom.<br>
-<strong>Assembly A:</strong> All 2nd period Business, English, Math, Music
-and Science classes.<br>
-<strong>Assembly B:</strong> All 2nd period Art, ECE, Foreign Language,
-Health/PE, Special Services, Social Studies and 700 building Science
-classes.<br>
-&nbsp;<br> `
+<p>All Students <strong>report to class at the beginning of the period</strong> and leave belongings in locked classroom.<br>
+<strong>Assembly A:</strong>  All 2nd period Business, English, Math, Music and Science classes.<br>
+<strong>Assembly B:</strong>  All 2nd period Art, ECE, Foreign Language, Health/PE, Special Services, Social Studies and 700 building Science classes.<br>
+&nbsp;<br>`
 document.getElementById("thesub").innerHTML = thehtml;
