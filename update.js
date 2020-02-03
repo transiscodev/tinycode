c4f1d6c0be37cb6bc8f00e4b0468ebb1cd616cfa
transiscodev <sirgjw@gmail.com>
2020-02-03 08:44:42 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-<br> <h1>Notice</h1><br>This service will soon be defunct. This service was originally created as a means of updating the Class of 2023 from the Bear crew.<br>Currently, this app serves as a way to post bell schedules.<br>A new services that specifically accomplishes this will be implemented soon and a link will be posted here.`;
+<br> <h1>Notice</h1><br>This service will soon be defunct. This service was originally created as a means of updating the Class of 2023 about schedules.<br>Currently, this app serve as a way to post bell schedules.<br>A new services that specifically accomplishes this will be implemented soon and a link will be posted here.`;
 document.getElementById("thesub").innerHTML = thehtml;
