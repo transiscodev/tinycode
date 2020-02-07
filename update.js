9ee9f30613fe1c095d95ca0ecda407a5ea853e2b
transiscodev <sirgjw@gmail.com>
2020-02-07 13:55:14 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,5 @@
 var thehtml = `
 <h3>Recent Updates (Online)</h3>
-<br> <h1>Get tickets to CLUE<h1><br>Click the "Event Tickets" tab, and then "Purple Pass OSD Theater".`;
+<br> <h1>Get tickets to CLUE<h1>
+Click the "Event Tickets" tab, and then "Purple Pass OSD Theater".`;
 document.getElementById("thesub").innerHTML = thehtml;
